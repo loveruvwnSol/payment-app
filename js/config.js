2f54836936f5e121ce0b41660f46d4d30379ffe2
loveruvwn <ocosio0907@gmail.com>
2021-08-20 18:57:33 +0900

    added system

--- js/config.js
+++ js/config.js
@@ -1,16 +1,15 @@
-// id取得
 class PaymentApp {
+  // id取得
   constructor() {
-    this.title = document.getElementById("outputTitle");  // タイトルテキスト
+    this.title = document.getElementById("outputTitle"); // タイトルテキスト
     this.select_text = document.getElementById("outputSelectText"); // サブテキスト
-    this.dutchTreatButton = document.getElementById("dutchTreatButton");  // 割り勘ボタン
+    this.dutchTreatButton = document.getElementById("dutchTreatButton"); // 割り勘ボタン
     this.graceButton = document.getElementById("graceButton"); // 奢りボタン
     this.amountOfMoneyBox = document.getElementById("amountOfMoneyBox"); // 金額入力テキストボックス
     this.DTNumberOfPeopleBox = document.getElementById("DTNumberOfPeopleBox"); // 割り勘人数入力テキストボックス
     this.dutchTreatDecisionButton = document.getElementById("dutchTreatDecisionButton"); // 割り勘結果表示ボタン
-    this.GRCNumberOfPeopleBox = document.getElementById("GRCNumberOfPeopleBox"); // 奢り人数入力テキストボックス
-    this.graceNextPageButton = document.getElementById("graceNextPageButton"); // 奢りページ遷移ボタン
-    this.inputName = document.getElementById("inputName");
+    this.addTextboxButton = document.getElementById("addTextboxButton");
+    this.inputName = document.getElementById("inputName"); // 名前入力テキストボックスを囲うdivId
     this.nameBox = document.getElementById("nameBox"); // 名前入力テキストボックス
     this.graceDecisionButton = document.getElementById("graceDecisionButton"); // 奢り結果表示ボタン
     this.resultText = document.getElementById("resultText"); // 結果テキスト
@@ -68,8 +67,7 @@ class PaymentApp {
       this.DTNumberOfPeopleBox.style.display = "flex";
       this.DTNumberOfPeopleBox.style.color = "#000000";
       this.errorText.innerHTML = "入力されていません";
-    }
-    else {
+    } else {
       this.errorText.innerHTML = "";
     }
   }
@@ -77,7 +75,7 @@ class PaymentApp {
   // 奢りボタンが押されたら実行
   startGrace() {
     // innerHTMLでテキストを書き換え
-    this.title.innerHTML = "人数を入力してください";
+    this.title.innerHTML = "名前を入力してください";
     this.select_text.innerHTML = "";
 
     // 割り勘ボタンと奢りボタンの削除
@@ -85,68 +83,72 @@ class PaymentApp {
     this.graceButton.style.display = "none";
 
     // textboxと決定ボタン、戻るボタンを追加
-    this.GRCNumberOfPeopleBox.style.display = "flex";
-    this.GRCNumberOfPeopleBox.style.color = "#000000";
-    this.backButton.style.display = "flex";
-    this.graceNextPageButton.style.display = "flex";
-  }
-
-  // ページ遷移ボタンが押されたら実行
-  GraceNextPage() {
-    // textboxに入力された数値を取得
-    const peopeNPO = this.GRCNumberOfPeopleBox.value;
-
-    // innerHTMLでテキストを書き換え
-    this.title.innerHTML = "名前を入力してください";
-
     this.nameBox.style.display = "flex";
     this.nameBox.style.color = "#000000";
+    this.backButton.style.display = "flex";
+    this.addTextboxButton.style.display = "flex"
     this.graceDecisionButton.style.display = "flex";
+  }
 
-    // textboxとページ遷移ボタンの削除
-    this.graceNextPageButton.style.display = "none";
-    this.GRCNumberOfPeopleBox.style.display = "none";
+  // textbox追加
+  addTextbox() {
+    // 新しい div 要素を作成
+    var newDiv = document.createElement("input");
+    newDiv.setAttribute("type", "text");
+    newDiv.setAttribute("id", "addNameBox");
+    newDiv.setAttribute("class", "textbox");
+    newDiv.setAttribute("placeholder", "名前を入力してください");
+    newDiv.style.display = "flex";
+    newDiv.style.color = "#000000";
 
-    // 数値が入力されなかったらエラーテキストを表示
-    if (peopeNPO == "") {
-      this.title.innerHTML = "人数を入力してください";
+    // DOM に新しく作られた要素とその内容を追加
+    var currentDiv = document.getElementById("inputName");
+    currentDiv.appendChild(newDiv);
+  }
+
+  // 奢りの結果
+  GraceSubmit() {
+
+    // innerHTMLでテキストを書き換え
+    this.title.innerHTML = "今回奢る人は";
+    this.resultText.innerHTML =  + "さん";
+
+    // textboxとボタンの削除
+    this.nameBox.style.display = "none";
+    this.addTextboxButton.style.display = "none"
+    this.graceDecisionButton.style.display = "none";
+    this.inputName.style.display = "none";
+
+    const NameArray = [
+      
+    ]
+    // const GraceResult
+    const Name = this.nameBox.value;
+    NameArray.push(Name);
+    console.log(NameArray);
+    
+    if (Name == ""){
+      this.title.innerHTML = "名前を入力してください";
       this.resultText.innerHTML = "";
-      this.GRCNumberOfPeopleBox.style.display = "flex";
-      this.GRCNumberOfPeopleBox.style.color = "#000000";
-      this.graceNextPageButton.style.display = "flex";
-      this.nameBox.style.display = "none";
-      this.graceDecisionButton.style.display = "none";
+      this.addTextboxButton.style.display = "flex";
+      this.nameBox.style.display = "flex";
+      this.inputName.style.display = "flex";
+      this.graceDecisionButton.style.display = "flex";
+      this.backButton.style.display = "flex";
       this.errorText.innerHTML = "入力されていません";
     }
     else {
       this.errorText.innerHTML = "";
     }
   }
-
-  // 奢りの結果
-  GraceSubmit() {
-
-
-    // if ( == ""){
-    //   this.title.innerHTML = "名前を入力してください";
-    //   this.resultText.innerHTML = "";
-    //   this.nameBox.style.display = "flex";
-    //   this.graceDecisionButton.style.display = "flex";
-    //   this.backButton.style.display = "flex";
-    //   this.errorText.innerHTML = "入力されていません";
-    // }
-    // else {
-    //   this.errorText.innerHTML = "";
-    // }
-  }
 }
 
 window.onload = function () {
   const app = new PaymentApp();
-  app.dutchTreatButton.addEventListener("click", () => app.startDutchTreat());
+  app.dutchTreatButton.addEventListener("click", () => app.startDutchTreat()); 
   app.dutchTreatDecisionButton.addEventListener("click", () => app.dutchTreatSubmit());
   app.graceButton.addEventListener("click", () => app.startGrace());
-  app.graceNextPageButton.addEventListener("click", () => app.GraceNextPage());
+  app.addTextboxButton.addEventListener("click", () => app.addTextbox());
   app.graceDecisionButton.addEventListener("click", () => app.GraceSubmit());
   app.backButton.addEventListener("click", () => location.reload());
 };
